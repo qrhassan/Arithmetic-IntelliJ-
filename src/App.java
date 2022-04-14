c3cac286a16c4ced508b9424de0a0b96a03ed90d
kazi.hassan <qrhassan@gmail.com>
2022-04-14 10:47:53 -0400

    Updated

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println(10+5);
+        System.out.println(9/3); //Divide
+
+        //Surprise
+        System.out.println(10/3); //Divide
+
+        System.out.println(10.0/3.0); //Divide
+        System.out.println(9/4.0); //one floating point value gives floating point results
+
+        System.out.println(2*2); //Multiply
+
+        System.out.println(8-2); //Minus
+    }
+}
